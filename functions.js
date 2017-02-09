72050fb9c4d90e5e552dc27827c46add21e940c7
jneeb <neebjustin@gmail.com>
2017-02-08 22:49:51 -0500

    added maps api to bottom of page

--- functions.js
+++ functions.js
@@ -14,3 +14,17 @@ function navFunction() {
 		nav.classList.remove('isOpen');
 	}
 }
+
+// google maps function
+
+function initMap() {
+        var uluru = {lat: 43.652881, lng: -79.379339};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
